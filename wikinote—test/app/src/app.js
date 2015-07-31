b218394e79f34b72e30876738fe3c84b28273248
Heon_Gu <514720858@qq.com>
2015-07-31 20:30:04 +0800

    test3

---
+++ wikinote—test/app/src/app.js
@@ -0,0 +1,29 @@
+require.config({
+  // Alwats make main.js the baseUrl
+  paths: {
+    marked: '../bower_components/marked/marked.min',
+    touch: './touch',
+    zepto: './zepto',
+    data: './data',
+    show: './show',
+  }
+});
+
+// Start the main app logic
+require(['zepto', 'show', 'touch'], function(Zepto, show, touch) {
+  show.showNotebooks();
+  show.showAllNotes();
+  show.searchByTitle();
+  show.createNotebook();
+  show.saveNote();
+  show.clickShowAllNote();
+  $('#closesearch').bind('click', function() {
+    $('#search-list').hide();
+  });
+
+  $('#returnNotebook').bind('tap', function() {
+    $('.contain')[0].style.webkitTransition = '-webkit-transform 0.2s ease-out';
+    $('.contain')[0].style.webkitTransform = 'translate3d(0, 0, 0)';
+  });
+
+});
